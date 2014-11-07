4d395f5bde33a382b7723a63de3f596d21f5e920
pdanshov <pdanshv@gmail.com>
2014-11-07 05:35:52 -0500

    Fri Nov  7 05:35:33 EST 2014

--- MDB-Test.pl
+++ MDB-Test.pl
@@ -1,26 +1,28 @@
 #!/usr/bin/perl
 use strict;use warnings;
 
+echo Test;
+print Test;
+
 use DBI;
-##use Win32::OLE;
+unshift(@INC, '/usr/lib/perl5/site_perl/5.8/Win32');
+use lib "/usr/lib/perl5/site_perl/5.8/Win32";
+use Win32::OLE;
 #use Win32OLE;
 
-#my $DBFile  = qw( X:\Path\To\Your\Database.mdb ); # 
-##my $DBFile  = qw( C:\Program Files\Trading Partner\System\Recon.mdb );
-#Choose appropriate version of Jet for your system
-#my $Jet = Win32::OLE->CreateObject('DAO.DBEngine.36')   or die "Can't create Jet database engine.";
+	#my $DBFile  = qw( X:\Path\To\Your\Database.mdb ); # 
+my $DBFile  = qw( C:\Program Files\Trading Partner\System\Recon.mdb );
+	#Choose appropriate version of Jet for your system
+	#my $Jet = Win32::OLE->CreateObject('DAO.DBEngine.36')   or die "Can't create Jet database engine.";
 ##my $Jet = Win32OLE->CreateObject('DAO.DBEngine.36')   or die "Can't create Jet database engine.";
-##my  $DB = $Jet->OpenDatabase( $DBFile );
-
-my $SQLquery = "DELETE * FROM Test_Table";
+my  $DB = $Jet->OpenDatabase( $DBFile );
+##my $SQLquery = "DELETE * FROM Test_Table";
 $DB->Execute($SQLquery, 128); #128=DBFailOnError
-
-#You don't even need to set up a DSN to talk to Access databases, you can simply pass the file name of the .mdb file:
-#$dbh = DBI->connect(q{dbi:ODBC:driver=Microsoft Access Driver (*.mdb); +dbq=\\fooserver\path\source.mdb}, '', '');
-#C:\Program Files\Trading Partner\System\Recon.mdb
-$dbh = DBI->connect(q{dbi:ODBC:driver=Microsoft Access Driver (*.mdb); +dbq=C:\Program Files\Trading Partner\System\Recon.mdb}, '', '');
-
-#my $dbh = DBI->connect('dbi:ODBC:driver=Microsoft Access Driver (*.mdb, *.accdb);dbq=[path to mdb file]') or die "\nCan't connect to Access database: " . $DBI::errstr;
+	#You don't even need to set up a DSN to talk to Access databases, you can simply pass the file name of the .mdb file:
+	#$dbh = DBI->connect(q{dbi:ODBC:driver=Microsoft Access Driver (*.mdb); +dbq=\\fooserver\path\source.mdb}, '', '');
+	#C:\Program Files\Trading Partner\System\Recon.mdb
+my $dbh = DBI->connect(q{dbi:ODBC:driver=Microsoft Access Driver (*.mdb); +dbq=C:\Program Files\Trading Partner\System\Recon.mdb}, '', '');
+my $dbh = DBI->connect('dbi:ODBC:driver=Microsoft Access Driver (*.mdb, *.accdb);dbq=[path to mdb file]') or die "\nCan't connect to Access database: " . $DBI::errstr;
 
 #This is a very basic very alpha quality Win32::ODBC emulation for the DBI. To use it just replace
 #use Win32::ODBC;
--- Perl.pl
+++ Perl.pl
@@ -4,8 +4,15 @@ perl -V
 
 I suspect it has something to do with the fact that all of your @INC pathways are pointing to your Perl 5.8 installation.
 You can add the Perl 5.10 installation to the @INC array by adding either:
-unshift(@INC, '/path/to/your/5.10/install');
+	unshift(@INC, '/path/to/your/5.10/install');
 or
-use lib "/path/to/your/5.10/install";
+	use lib "/path/to/your/5.10/install";
 to the beginning of your script, before the 'use' line for the troublesome module.
+
+What is the output of perl -v?
+It looks like your Win32::ODBC was compiled for perl 5.8.2 whereas you
+have perl 5.8.5. You need probably need to reinstall Win32::ODBC.
+
+Win32 Error Codes:
+http://msdn.microsoft.com/en-us/library/windows/desktop/ms681382%28v=vs.85%29.aspx
 
